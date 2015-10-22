392fbf9455eda658493f64409e82f31983b1f2a5
Ubuntu <ubuntu@ip-172-31-16-242.ap-southeast-1.compute.internal>
2015-10-22 06:27:39 +0000

    adding server

---
+++ server/aggregate.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+from cubes import Workspace, Cell, PointCut
+
+# 1. Create a workspace
+workspace = Workspace()
+workspace.register_default_store("sql", url="sqlite:///data.sqlite")
+workspace.import_model("model.json")
+
+# 2. Get a browser
+browser = workspace.browser("airquality")
+
+# 3. Play with aggregates
+result = browser.aggregate()
+
+print("Total\n"
+      "----------------------")
+
+print("Record count : %8d" % result.summary["record_count"])
+print("Total amount : %8d" % result.summary["average_mean"])
+
+#
+# 4. Drill-down through a dimension
+#
+"""
+print("\n"
+      "Drill Down by Category (top-level Item hierarchy)\n"
+      "==================================================")
+#
+result = browser.aggregate(drilldown=["location"])
+#
+print(("%-20s%10s%10s%10s\n"+"-"*50) % ("Category", "Count", "Total", "Double"))
+#
+for row in result.table_rows("item"):
+    print("%-20s%10d%10d%10d" % ( row.label,
+                              row.record["record_count"],
+                              row.record["amount_sum"],
+                              row.record["double_amount_sum"])
+                              )
+
+print("\n"
+      "Slice where Category = Equity\n"
+      "==================================================")
+
+cut = PointCut("location", ["e"])
+cell = Cell(browser.cube, cuts = [cut])
+
+result = browser.aggregate(cell, drilldown=["location"])
+
+print(("%-20s%10s%10s%10s\n"+"-"*50) % ("Sub-category", "Count", "Total", "Double"))
+
+for row in result.table_rows("item"):
+    print("%-20s%10d%10d%10d" % ( row.label,
+                              row.record["record_count"],
+                              row.record["amount_sum"],
+                              row.record["double_amount_sum"],
+                              ))
+                              """
---
+++ server/backup/prepare_data.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Data preparation for the hello_world example
+from __future__ import print_function
+
+from sqlalchemy import create_engine
+from cubes.tutorial.sql import create_table_from_csv
+
+# 1. Prepare SQL data in memory
+
+FACT_TABLE = "airquality"
+
+print("preparing data...")
+
+engine = create_engine('sqlite:///data.sqlite')
+
+create_table_from_csv(engine,
+                      "data.csv",
+                      table_name=FACT_TABLE,
+                      fields=[
+                            ("state_code", "integer"),
+                            ("county_code", "integer"),
+                            ("site_num", "integer"),
+                            ("parameter_code", "integer"),
+                            ("poc", "integer"),
+                            ("datum", "string"),
+                            ("parameter_name", "string"),
+                            ("sample_duration", "string"),
+                            ("pollutant_standard", "string"),
+                            ("metric_used", "string"),
+                            ("method_name", "string"),
+                            ("year", "integer"),
+                            ("measure_unit", "string"),
+                            ("event_type", "string"),
+                            ("observation_count", "integer"),
+                            ("observation_percent1", "integer"),
+                            ("valid_day_count", "integer"),
+                            ("required_day_count", "integer"),
+                            ("exceptional_data_count", "integer"),
+                            ("null_data_count", "integer"),
+                            ("primary_exceedence_count", "integer"),
+                            ("secondary_exceedence_count", "integer"),
+                            ("certification_indicator", "string"),
+                            ("num_obs_below_mdl", "integer"),
+                            ("arithmetic_mean", "float"),
+                            ("arithmetic_stdev", "float"),
+                            ("max1", "float"),
+                            ("max1_datetime", "text"),
+                            ("max2", "float"),
+                            ("max2_datetime", "text"),
+                            ("max3", "float"),
+                            ("max3_datetime", "text"),
+                            ("max4", "float"),
+                            ("max4_datetime", "text"),
+                            ("max1_non_overlapping", "integer"),
+                            ("max1_non_overlapping_datetime", "text"),
+                            ("max2_non_overlapping", "integer"),
+                            ("max2_non_overlapping_datetime", "text"),
+                            ("percentile_99", "float"),
+                            ("percentile_98", "float"),
+                            ("percentile_95", "float"),
+                            ("percentile_90", "float"),
+                            ("percentile_75", "float"),
+                            ("percentile_50", "float"),
+                            ("percentile_10", "float"),
+                            ("local_site", "string"),
+                            ("state", "string"),
+                            ("county_name", "string"),
+                            ("city", "string"),
+                            ("cbsa_name", "string"),
+                            ("date_of_last_change", "text"),
+                            ("subarea", "string")],
+                      create_id=True
+                  )
+
+print("done. file data.sqlite created")
